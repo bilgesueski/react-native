18ec034b1db8d45b557b2cc34efb107a42df45f8
bilgesueski <bilgesuesk@gmail.com>
2024-06-22 15:01:20 +0300

    add SearchScreen

--- restaurant-guide/screens/SearchScreen.js
+++ restaurant-guide/screens/SearchScreen.js
@@ -1,12 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import SearchBar from "../components/SearchBar";
 
 export default function SearchScreen() {
   return (
     <View>
-      <Text>SearchScreen</Text>
+      <SearchBar />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
