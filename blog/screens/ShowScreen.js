bc6bf159a421fdf271c83c9d2041d8a2bc40636d
bilgesueski <bilgesuesk@gmail.com>
2024-07-12 16:49:29 +0300

    style: Update styles

--- blog/screens/ShowScreen.js
+++ blog/screens/ShowScreen.js
@@ -6,10 +6,41 @@ export default function ShowScreen({ route }) {
   const { state } = useContext(Context);
   const blogPost = state.find((blogPost) => blogPost.id === route.params.id);
   return (
-    <View>
-      <Text>{blogPost.title}</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Title</Text>
+        <Text style={styles.content}>{blogPost.title}</Text>
+      </View>
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Content</Text>
+        <Text style={styles.content}>{blogPost.content}</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    alignItems: "center",
+    marginTop: 10,
+  },
+  container: {
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingBottom: 10,
+    borderRadius: 30,
+    alignItems: "center",
+    width: "80%",
+    backgroundColor: "#BDD3C7",
+  },
+  title: {
+    fontSize: 24,
+    color: "#081D56",
+    padding: 8,
+  },
+  content: {
+    fontSize: 18,
+    color: "#081D56",
+  },
+});
