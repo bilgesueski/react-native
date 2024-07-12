c6dc1eb3c4dc6920a58414692c1232e4c53bfd3f
bilgesueski <bilgesuesk@gmail.com>
2024-07-12 17:22:48 +0300

    Create EditScreen

---
+++ blog/screens/EditScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useContext } from "react";
+import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
+
+export default function EditScreen({ route }) {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find((blogPost) => blogPost.id === route.params.id);
+  return (
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({});
