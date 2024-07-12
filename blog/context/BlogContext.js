57829f3412cb572b131ad90663633cef828ff961
bilgesueski <bilgesuesk@gmail.com>
2024-07-12 17:50:34 +0300

    feat: Add edit_blogpost action

--- blog/context/BlogContext.js
+++ blog/context/BlogContext.js
@@ -63,6 +63,11 @@ const blogReducer = (state, action) => {
 
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
+
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     default:
       return state;
   }
@@ -83,8 +88,17 @@ const deleteBlogPost = (dispatch) => {
   };
 };
 
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   []
 );
--- blog/screens/EditScreen.js
+++ blog/screens/EditScreen.js
@@ -4,14 +4,18 @@ import { useContext } from "react";
 import { Context } from "../context/BlogContext";
 import BlogPostForm from "../components/BlogPostForm";
 
-export default function EditScreen({ route }) {
-  const { state } = useContext(Context);
+export default function EditScreen({ navigation, route }) {
+  const { state, editBlogPost } = useContext(Context);
+  const id = route.params.id;
 
   const blogPost = state.find((blogPost) => blogPost.id === route.params.id);
   return (
     <BlogPostForm
       isEditable={true}
       initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.pop());
+      }}
     />
   );
 }
