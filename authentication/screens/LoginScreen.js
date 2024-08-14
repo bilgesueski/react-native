683d2d0983958c86936ecdf96650160c34c13290
bilgesueski <bilgesuesk@gmail.com>
2024-08-14 16:48:39 +0300

    add style

--- authentication/screens/LoginScreen.js
+++ authentication/screens/LoginScreen.js
@@ -1,12 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
 
 export default function LoginScreen() {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="E-Mail" />
+        <TextInput style={styles.input} placeholder="Password" />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Log In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.outlineButton]}>
+          <Text style={styles.outlineButtonText}>Sign In</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: "60%",
+    marginTop: 30,
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: 700,
+  },
+  outlineButton: {
+    marginTop: 10,
+    backgroundColor: "white",
+  },
+  outlineButtonText: {
+    color: "#0782F9",
+    fontSize: 16,
+    fontWeight: 700,
+  },
+});
