3f93a323ba3129bbb2ef9451485ea9264b47c5f6
bilgesueski <bilgesuesk@gmail.com>
2024-08-16 17:13:21 +0300

    chore: Add sign out functionality

--- authentication/screens/HomeScreen.js
+++ authentication/screens/HomeScreen.js
@@ -1,12 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { auth } from "../firebase";
+import { useNavigation } from "@react-navigation/native";
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.navigate("Login");
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Text>E-Mail: {auth.currentUser?.email}</Text>
+      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+        <Text style={styles.buttonText}>Sign Out</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    padding: 15,
+    alignItems: "center",
+    borderRadius: 10,
+    width: "60%",
+    marginTop: 50,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: 700,
+  },
+});
