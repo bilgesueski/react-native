e4bf7638de204d8b45f069bb69cc4669907dca48
bilgesueski <bilgesuesk@gmail.com>
2024-08-16 17:12:54 +0300

    chore: Add useEffect hook

--- authentication/screens/LoginScreen.js
+++ authentication/screens/LoginScreen.js
@@ -6,7 +6,7 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth } from "../firebase";
 import { useNavigation } from "@react-navigation/native";
 
